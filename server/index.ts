be0d8b10b10c1f7718745a57da9dc3db24ee56c4
Vishwas Palaksha <51168621-vishwaspalaksha@users.noreply.replit.com>
2026-03-12 18:40:21 +0000

    fix railway session cookies

--- server/index.ts
+++ server/index.ts
@@ -9,6 +9,8 @@ import pg from "pg";
 const app = express();
 const httpServer = createServer(app);
 
+app.set("trust proxy", 1);
+
 declare module "http" {
   interface IncomingMessage {
     rawBody: unknown;
@@ -82,11 +84,12 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      secure: false,
       httpOnly: true,
-      maxAge: 1000 * 60 * 60, // 1 hour
+      sameSite: "lax",
+      maxAge: 1000 * 60 * 60,
     },
-  })
+  }),
 );
 
 (async () => {
